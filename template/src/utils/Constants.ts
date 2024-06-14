27486c411cb4dd9e07d30370fba786f4cf155f93
summangal <summanga@cisco.com>
2024-06-14 13:33:54 +0530

    Delete Constants.ts

--- template/src/utils/Constants.ts
+++
@@ -1,7 +0,0 @@
-import { IConfigData } from '@interfaces/IConfiguration';
-
-export const API_JSON_CONFIG: IConfigData = {
-  okta: { issuer: '', clientId: '' },
-  backendUrlConfig: { prometheusUrl: '', phanesUrl: '' },
-};
-export const ACTIVE = 'ACTIVE';
