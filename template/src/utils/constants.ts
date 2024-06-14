cdb7081aee4d602dd152e4d10125d3fde4b211ee
summangal <summanga@cisco.com>
2024-06-14 13:34:16 +0530

    Create constants.ts

---
+++ template/src/utils/constants.ts
@@ -0,0 +1,7 @@
+import { IConfigData } from '@interfaces/IConfiguration';
+
+export const API_JSON_CONFIG: IConfigData = {
+    okta: { issuer: '', clientId: '' },
+    backendUrlConfig: { prometheusUrl: '', phanesUrl: '' },
+};
+export const ACTIVE = 'ACTIVE';
